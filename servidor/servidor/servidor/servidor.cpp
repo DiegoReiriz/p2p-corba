ccbf3ea6a724e26315f16c520d271f13bd9746e0
Diego Reiriz Cores <diegoreiriz@gmail.com>
2016-04-10 01:07:25 +0200

    son capaz de mandar unha estructura usuario do cliente ao servidor

--- servidor/servidor/servidor/servidor.cpp
+++ servidor/servidor/servidor/servidor.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include "echo.hh"
+#include "interface.hh"
 #include <ios>
 #include <iostream>
 #include "SQLite.h"
@@ -27,6 +28,50 @@ char* Echo_i::echoString(const char* mesg)
 }
 
 //////////////////////////////////////////////////////////////////////
+class userManager_i : public POA_chat::userManager
+{
+	public :
+		inline userManager_i() {}
+		virtual ~userManager_i() {}
+		virtual ::CORBA::Boolean signIn(const ::chat::VOUser& usuario);
+		virtual ::CORBA::Boolean signOut(const ::chat::VOUser& usuario);
+		virtual ::CORBA::Boolean signUp(const ::chat::VOUser& usuario);
+		virtual ::CORBA::Boolean alterUser(const ::chat::VOUser& usuario);
+		virtual ::chat::listaUsuarios* getFrindList(const ::chat::VOUser& usuario);
+
+};
+
+::CORBA::Boolean userManager_i::signIn(const ::chat::VOUser& usuario) {
+	::CORBA::Boolean res = false;
+
+	cout << usuario.id << endl;
+	cout << usuario.nombre << endl;
+	cout << usuario.email << endl;
+	cout << usuario.hash << endl;
+	cout << usuario.salt << endl;
+	cout << usuario.avatar << endl;
+
+	return res;
+}
+::CORBA::Boolean userManager_i::signOut(const ::chat::VOUser& usuario) {
+	::CORBA::Boolean res = false;
+	return res;
+}
+::CORBA::Boolean userManager_i::signUp(const ::chat::VOUser& usuario) {
+	::CORBA::Boolean res = false;
+	return res;
+}
+::CORBA::Boolean userManager_i::alterUser(const ::chat::VOUser& usuario) {
+	::CORBA::Boolean res = false;
+	return res;
+}
+::chat::listaUsuarios* userManager_i::getFrindList(const ::chat::VOUser& usuario) {
+	::chat::listaUsuarios lista;
+	return &lista;
+}
+
+
+//////////////////////////////////////////////////////////////////////
 
 int main(int argc, char **argv){
 	try {
@@ -34,13 +79,16 @@ int main(int argc, char **argv){
 		CORBA::Object_var       obj = orb->resolve_initial_references("RootPOA");
 		PortableServer::POA_var poa = PortableServer::POA::_narrow(obj);
 
-		PortableServer::Servant_var<Echo_i> myecho = new Echo_i();
+		//PortableServer::Servant_var<Echo_i> myecho = new Echo_i();
+		PortableServer::Servant_var<userManager_i> myuserManager = new userManager_i();
 
-		PortableServer::ObjectId_var myechoid = poa->activate_object(myecho);
+		//PortableServer::ObjectId_var myechoid = poa->activate_object(myecho);
+		PortableServer::ObjectId_var myuserManagerid = poa->activate_object(myuserManager);
 
 		// Obtain a reference to the object, and register it in
 		// the naming service.
-		obj = myecho->_this();
+		//obj = myecho->_this();
+		obj = myuserManager->_this();
 
 		CORBA::String_var sior(orb->object_to_string(obj));
 		cout << sior << endl;
@@ -109,7 +157,7 @@ static CORBA::Boolean bindObjectToName(CORBA::ORB_ptr orb, CORBA::Object_ptr obj
 		// Bind objref with name Echo to the testContext:
 		CosNaming::Name objectName;
 		objectName.length(1);
-		objectName[0].id	= (const char*) "Echo";   // string copied
+		objectName[0].id	= (const char*) "User";   // string copied
 		//objectName[0].kind = (const char*) "Object"; // string copied
 		
 		try {
--- testNameServiceCorba/src/com/company/Main.java
+++ testNameServiceCorba/src/com/company/Main.java
@@ -2,6 +2,9 @@ package com.company;
 
 import ECHOAPP.Echo;
 import ECHOAPP.EchoHelper;
+import chat.VOUser;
+import chat.userManager;
+import chat.userManagerHelper;
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.*;
 
@@ -21,14 +24,25 @@ public class Main
                     orb.resolve_initial_references("NameService");
             // Use NamingContextExt instead of NamingContext. This is
             // part of the Interoperable naming Service.
+//            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+//
+//            // resolve the Object Reference in Naming
+//            String name = "Echo";
+//            Echo e= EchoHelper.narrow(
+//                    ncRef.resolve_str(name)
+//            );
+//            e.echoString("FURRULA");
+
             NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
 
             // resolve the Object Reference in Naming
-            String name = "Echo";
-            Echo e= EchoHelper.narrow(
+            String name = "User";
+            userManager um= userManagerHelper.narrow(
                     ncRef.resolve_str(name)
             );
-            e.echoString("FURRULA");
+
+            um.signIn(new VOUser((short)1,"nombre","email","hash","salt","avatar"));
+
 
 
         } catch (Exception e) {
