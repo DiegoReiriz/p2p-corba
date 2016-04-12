e4629e3f9a0dd25ebfc9204cadb6fc87a18b96ba
Carlos Parada Alfaya <carlos.p.a123@gmail.com>
2016-04-12 11:30:19 +0200

    Interfaces Gráficas
    Interfaces Gráficas hechas, ahora el proyecto de cliente arranca con la
    interfaz de cliente

--- cliente/src/sample/Main.java
+++ cliente/src/sample/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Inicio.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("CHAtty");
+        primaryStage.setScene(new Scene(root, 282, 194));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
